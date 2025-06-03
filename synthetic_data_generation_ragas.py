55f17a9612e3c2dc0b6fd10993682269a037d665
noobEic <yuzx25@mail2.sysu.edu.cn>
2025-06-03 10:49:15 +0800

    modified:   synthetic_data_generation_ragas.py

--- synthetic_data_generation_ragas.py
+++ synthetic_data_generation_ragas.py
@@ -6,8 +6,8 @@ from ragas.embeddings import LangchainEmbeddingsWrapper
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from ragas.testset import TestsetGenerator
-
-from api import BASE_URL, API_KEY
+from langchain_ollama import OllamaEmbeddings
+from api import BASE_URL, API_KEY, OPENAI_API_KEY,OPENAI_BASE_URL
 path = "reference/"
 loader = DirectoryLoader(path, glob="**/*.doc")
 docs = loader.load()
@@ -16,12 +16,15 @@ print(docs.__len__())
 
 
 generator_llm = LangchainLLMWrapper(ChatOpenAI(model="deepseek-r1",base_url=BASE_URL,api_key=API_KEY))
-generator_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings(model="deepseek-r1",base_url=BASE_URL,api_key=API_KEY))
+generator_embeddings = LangchainEmbeddingsWrapper(OllamaEmbeddings(
+    model="bge-large",
+    base_url="http://localhost:11434"
+))
 
 generator = TestsetGenerator(llm=generator_llm,embedding_model=generator_embeddings)
 dataset = generator.generate_with_langchain_docs(docs, testset_size=10)
 
 
-dataset.to_pandas()
-dataset.to_csv("synthetic_data_generation_ragas.csv", index=False)
+
+dataset.to_csv("synthetic_data_generation_ragas.csv")
 print("Synthetic data generation completed and saved to 'synthetic_data_generation_ragas.csv'.")
