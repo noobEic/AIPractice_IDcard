07aad1cca6ad8823aac71c0bc0c3289fb1e605ec
noobEic <yuzx25@mail2.sysu.edu.cn>
2025-06-03 15:28:19 +0800

    modified:   synthetic_data_generation_ragas.py

--- synthetic_data_generation_ragas.py
+++ synthetic_data_generation_ragas.py
@@ -7,22 +7,39 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from ragas.testset import TestsetGenerator
 from langchain_ollama import OllamaEmbeddings
+from ragas.testset.persona import Persona
 from api import BASE_URL, API_KEY, OPENAI_API_KEY,OPENAI_BASE_URL
-path = "reference/"
-loader = DirectoryLoader(path, glob="**/*.doc")
-docs = loader.load()
 
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
+from splitter import LawSplitter
+loader = UnstructuredWordDocumentLoader("reference/关于传发广东省户口居民身份证管理工作操作规范（2024年版）的通知【正文】4433808.doc")
+documents = loader.load()
+splitter = LawSplitter()
+docs = splitter.split_documents(documents)
 print(docs.__len__())
-
 
 generator_llm = LangchainLLMWrapper(ChatOpenAI(model="deepseek-r1",base_url=BASE_URL,api_key=API_KEY))
 generator_embeddings = LangchainEmbeddingsWrapper(OllamaEmbeddings(
     model="bge-large",
     base_url="http://localhost:11434"
 ))
+personas = [
+    Persona(
+        name="基础咨询者",
+        role_description="办理身份证时询问基础政策信息（如有效期、费用等）的普通市民",
+    ),
+    Persona(
+        name="流程咨询者",
+        role_description="需要详细了解跨省办理、特殊情形流程等复杂问题的市民",
+    ),
+    Persona(
+        name="政策核实者",
+        role_description="对已获取的身份证办理政策信息进行真实性核对的谨慎市民",
+    )
+]
 
-generator = TestsetGenerator(llm=generator_llm,embedding_model=generator_embeddings)
-dataset = generator.generate_with_langchain_docs(docs, testset_size=10)
+generator = TestsetGenerator(llm=generator_llm,embedding_model=generator_embeddings,persona_list=personas)
+dataset = generator.generate_with_langchain_docs(docs, testset_size=2000)
 
 
 
--- vector_db.py
+++ vector_db.py
@@ -3,9 +3,11 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
 import os
 from typing import List
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
+from splitter import LawSplitter
 
 def process_and_store(docs: List[Document], 
-                      embedding_model: str = "BAAI/bge-small-zh",
+                      embedding_model: str = "BAAI/bge-large-zh",
                       persist_dir: str = "./vector_db"):
 
     embeddings = HuggingFaceEmbeddings(
@@ -29,10 +31,10 @@ def process_and_store(docs: List[Document],
 
 if __name__ == "__main__":
 
-    docs = [
-        Document(page_content="testtest", metadata={"source": "test1"}),
-        Document(page_content="啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊", metadata={"source": "test2"})
-    ]
+    loader = UnstructuredWordDocumentLoader("reference/关于传发广东省户口居民身份证管理工作操作规范（2024年版）的通知【正文】4433808.doc")
+    documents = loader.load()
+    splitter = LawSplitter()
+    docs = splitter.split_documents(documents)
     
     persist_dir = "./test_vector_db"
     vector_db = process_and_store(docs,persist_dir=persist_dir)
